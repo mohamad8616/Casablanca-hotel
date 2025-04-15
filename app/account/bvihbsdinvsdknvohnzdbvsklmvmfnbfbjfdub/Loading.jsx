9cb28fbf5ecdd2abf0a9ce66745b1422ad8e2833
mohamad8616 <mohamad.sh.a9036@gmail.com>
2025-04-16 02:09:58 +0330

    fixed the spinners

--- app/account/bvihbsdinvsdknvohnzdbvsklmvmfnbfbjfdub/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import { MoonLoader } from "react-spinners";
-
-export default function Loading() {
-  return (
-    <div className="z-10 flex h-screen w-full items-center justify-center">
-      <MoonLoader size={60} color="#312b2b4c" loading={true} />
-    </div>
-  );
-}
--- app/cabins/[cabinId]/Loading.jsx
+++
@@ -1,9 +0,0 @@
-import { MoonLoader } from "react-spinners";
-
-export default function Loading() {
-  return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <MoonLoader size={50} color="#000" loading={true} />
-    </div>
-  );
-}
